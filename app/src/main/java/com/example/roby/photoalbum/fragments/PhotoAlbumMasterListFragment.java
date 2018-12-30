1775ac6fbf4d2d101ef1dbcf42501c9425305ae3
rflorian2017 <roby_florian@yahoo.com>
2018-12-30 14:54:17 +0200

    Added code for master list fragment

--- app/src/main/java/com/example/roby/photoalbum/fragments/PhotoAlbumMasterListFragment.java
+++ app/src/main/java/com/example/roby/photoalbum/fragments/PhotoAlbumMasterListFragment.java
@@ -1,7 +1,35 @@
 package com.example.roby.photoalbum.fragments;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 
 public class PhotoAlbumMasterListFragment extends Fragment {
+    // Define a new interface OnImageClickListener that triggers a callback in the host activity
+    OnImageClickListener mCallback;
+
+    // OnImageClickListener interface, calls a method in the host activity named onImageSelected
+    public interface OnImageClickListener {
+        void onImageSelected(int position);
+    }
+
+    // Override onAttach to make sure that the container activity has implemented the callback
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        // This makes sure that the host activity has implemented the callback interface
+        // If not, it throws an exception
+        try {
+            mCallback = (OnImageClickListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement OnImageClickListener");
+        }
+    }
+
+    public PhotoAlbumMasterListFragment() {
+
+    }
+
 
 }
